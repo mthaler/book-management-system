9f4069323dc18dc65924c0f21afcc846bf8a184d
Michael Thaler <michael.thaler@dlr.de>
2021-06-09 11:05:14 +0200

    Added database.go.

--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/mthaler/book-managment-system/book"
+	"github.com/mthaler/book-managment-system/database"
 )
 
-func helloWorld(c *fiber.Ctx) {
-	c.Send("Hello, World!")
-}
-
 func setupRoutes(app *fiber.App) {
-	app.Get("/", helloWorld)
-
 	app.Get("/api/v1/book", book.GetBooks)
 	app.Get("/api/v1/book/:id", book.GetBook)
 	app.Post("/api/v1/book", book.NewBook)
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
+
 func main() {
 	app := fiber.New()
+	initDatabase()
 
 	setupRoutes(app)
 	app.Listen(3000)
+
+	defer database.DBConn.Close()
 }
